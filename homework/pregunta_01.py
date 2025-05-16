73a57c7e92c39be392d672fe5540df70fa6ea114
drifterDev <maalvarezmu@unal.edu.co>
2025-05-16 11:53:14 -0500

    Lab finalizado

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -13,3 +13,71 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+
+    import pandas as pd
+    import os
+
+    archivo_entrada = "files/input/solicitudes_de_credito.csv"
+    archivo_salida = "files/output/solicitudes_de_credito.csv"
+
+    datos = pd.read_csv(archivo_entrada, sep=";")
+
+    datos = datos.dropna()
+    datos["sexo"] = datos["sexo"].str.lower()
+    datos["tipo_de_emprendimiento"] = datos["tipo_de_emprendimiento"].str.lower()
+
+    datos["idea_negocio"] = (
+        datos["idea_negocio"]
+        .str.lower()
+        .str.replace("_", " ", regex=False)
+        .str.replace("-", " ", regex=False)
+        .str.strip()
+    )
+
+    datos["barrio"] = (
+        datos["barrio"]
+        .str.lower()
+        .str.replace("_", " ", regex=False)
+        .str.replace("-", " ", regex=False)
+    )
+
+    def normalizar_fecha(fecha):
+        partes = fecha.split("/")
+        if len(partes[0]) == 4:
+            return f"{partes[2]}/{partes[1]}/{partes[0]}"
+        return fecha
+
+    datos["fecha_de_beneficio"] = datos["fecha_de_beneficio"].apply(normalizar_fecha)
+
+    datos["monto_del_credito"] = (
+        datos["monto_del_credito"]
+        .str.replace(" ", "", regex=False)
+        .str.replace("$", "", regex=False)
+        .str.replace(",", "", regex=False)
+        .astype(float)
+    )
+
+    datos["línea_credito"] = (
+        datos["línea_credito"]
+        .str.lower()
+        .str.replace("_", " ", regex=False)
+        .str.replace("-", " ", regex=False)
+    )
+
+    columnas_unicas = [
+        "sexo",
+        "tipo_de_emprendimiento",
+        "idea_negocio",
+        "barrio",
+        "estrato",
+        "comuna_ciudadano",
+        "fecha_de_beneficio",
+        "monto_del_credito",
+        "línea_credito"
+    ]
+    datos = datos.drop_duplicates(subset=columnas_unicas)
+
+    os.makedirs(os.path.dirname(archivo_salida), exist_ok=True)
+    datos.to_csv(archivo_salida, sep=";", index=False)
+
+pregunta_01()
